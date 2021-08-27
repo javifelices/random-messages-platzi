0044e0e3d13837c609a2abb79169d40446f48465
Javi Felices <javifelices@gmail.com>
2021-08-27 18:46:42 +0200

    Añadir más mensajes (nombres)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const messages = ['Javi', 'Berta', 'Francisco', 'Victoria', 'Pedro', 'Carmen'];
+const messages = ['Javi', 'Berta', 'Francisco', 'Victoria', 'Pedro', 'Carmen', 'Rosa', 'Juan', 'Raúl', 'Ana'];
 
 const randomMsg = () => {
     const message = messages[Math.floor(Math.random() * messages.length)];
